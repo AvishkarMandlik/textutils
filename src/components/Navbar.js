e77c01f20c790d4215d55ce8320a5714ae7d30ef
AvishkarMandlik <mandlikavi121@gmail.com>
2024-01-14 09:44:39 +0530

    using props navbar title added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
     <div>
          <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/"><b className='grays'>TextUtils</b></a>
+    <a className="navbar-brand" href="/"><b className='grays'>{props.title}</b></a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
