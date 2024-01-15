9d5e09c743bd24f33101bfe3307fe456d34ef26f
AvishkarMandlik <mandlikavi121@gmail.com>
2024-01-15 11:10:20 +0530

    textForm added

---
+++ src/components/TextForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function TextForm(props) {
+
+  return (
+
+    <div>
+        <div className ="mb-3">
+            <h1>{props.heading}</h1>
+            <textarea className="form-control" id="myBox" rows="8" ></textarea>
+        </div>
+    </div>
+  )
+}
