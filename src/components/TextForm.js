ce8fcae2594df0bb3166a2570e6b1c72b6140afc
AvishkarMandlik <mandlikavi121@gmail.com>
2024-01-16 20:49:27 +0530

    convert to uppercase btn and logic set

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function TextForm(props) {
+  const [text, setText] = useState("")
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase()
+    setText(newText)
+  }
+  
+  
+  const handleOnChange = (event) => {
+    console.log("you click on uppercase")
+    setText(event.target.value)
+  }
+  
 
   return (
 
     <div>
         <div className ="mb-3">
-            <h1>{props.heading}</h1>
-            <textarea className="form-control" id="myBox" rows="8" ></textarea>
+            <h1>{props.heading} </h1>
+            <textarea className="form-control" placeholder='Enter your text here...' id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
         </div>
+        <button className="btn btn-outline-primary" onClick={handleUpClick}>Convert To UPR</button>
     </div>
   )
 }
