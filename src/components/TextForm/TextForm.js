05dba40a55fa8652a62cb580f8ac4f6218179787
AvishkarMandlik <mandlikavi121@gmail.com>
2024-01-18 06:30:55 +0530

    feat:word  char and time count added

--- src/components/TextForm/TextForm.js
+++ src/components/TextForm/TextForm.js
@@ -14,7 +14,6 @@ export default function TextForm(props) {
     console.log("you click on uppercase")
     setText(event.target.value)
   }
-  
 
   return (
 
@@ -24,9 +23,13 @@ export default function TextForm(props) {
             <textarea className="form-control" placeholder='Enter your text here...' id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
         </div>
         <button className="btn btn-outline-primary" onClick={handleUpClick}>Convert To UPR</button>
-        <div className="container my-3 d-flex align-items-center flex-column">
-          <h1>Your Text Summary</h1><br />
-          <p className='fs-3'><span className='string-words'>{text.split(" ").length}</span> Words &<span className='string-char'> {text.length} Characters</span></p>
+        <div className="container my-2 d-flex align-items-center flex-column">
+          <h2>Your Text Summary</h2><br />
+          <p className='fs-5'><span className='string-words'>{text.split(" ").length}</span> Words &
+          <span className='string-char'> {text.length} Characters</span><br />
+          <span className='minutes-to-read'>{0.008 * text.split(" ").length} Minutes To Read</span>
+          </p>
+
         </div>
     </div>
   )
