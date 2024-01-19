f664d6139fe27fc40757c08402f0fde85dbca04e
AvishkarMandlik <mandlikavi121@gmail.com>
2024-01-19 14:54:13 +0530

    feat:convert to lowercase added

--- src/components/TextForm/TextForm.js
+++ src/components/TextForm/TextForm.js
@@ -8,7 +8,10 @@ export default function TextForm(props) {
     let newText = text.toUpperCase()
     setText(newText)
   }
-  
+  const handleLwClick = () => {
+    let newText = text.toLowerCase()
+    setText(newText)
+  }
   
   const handleOnChange = (event) => {
     console.log("you click on uppercase")
@@ -22,7 +25,8 @@ export default function TextForm(props) {
             <h1>{props.heading} </h1>
             <textarea className="form-control" placeholder='Enter your text here...' id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
         </div>
-        <button className="btn btn-outline-primary" onClick={handleUpClick}>Convert To UPR</button>
+        <button className="btn btn-outline-primary mx-2" onClick={handleUpClick}>Convert To UPR</button>
+        <button className="btn btn-outline-primary mx-2" onClick={handleLwClick}>Convert To LWR</button>
         <div className="container my-2 d-flex align-items-center flex-column">
           <h2>Your Text Summary</h2><br />
           <p className='fs-5'><span className='string-words'><b>{text.split(" ").length}</b></span> Words &
