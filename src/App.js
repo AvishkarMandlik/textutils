3cba0b5143d4ed58055db3ab22dee4b0c3c89638
AvishkarMandlik <mandlikavi121@gmail.com>
2024-02-02 06:41:11 +0530

    show alert function added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
 import React,{useState} from 'react';
 import './App.css';
-import About from './components/About/About';
 import Navbar from './components/Navbar/Navbar';
 import TextForm from './components/TextForm/TextForm';
+import Alert from './components/Alert/Alert';
 
 function App() {
   const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+      setAlert({
+        msg: message,
+        type: type
+      })
+      setTimeout(() => {
+          setAlert(null);
+      }, 1500);
+  }
+
 
   const toggleMode = () =>{
     if(mode === "light"){
       setMode("dark");
       document.body.style.backgroundColor = '#042743';
-      // showAlert("Dark mode has been enabled", "success");
+      showAlert("Dark mode has been Enabled", "success");
     }
     else{
       setMode("light");
       document.body.style.backgroundColor = 'white';
-      // showAlert("Light mode has been enabled", "success");
+      showAlert("Light mode has been Enabled", "success");
     }
   }
 
   return (    
    <>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container">
      <TextForm heading="Enter the Text to Analyze Below" mode={mode}/>
      {/* <About /> */}
