67d763a9a6aef849ddaef8748e03159f4f8f9f3d
AvishkarMandlik <mandlikavi121@gmail.com>
2024-02-05 21:47:49 +0530

    remove about component dark mode code

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,36 +1,13 @@
-import React,{useState} from 'react' 
+import React from 'react' 
 import './About.css'
 
 export default function About() {
-    const [myStyle, setStyle] = useState({
-        backgroundColor:"black",
-        color: "white"
-    })
-
-    const [btnText, setbtnText] = useState("Enable Dark Mode")
-
-    const toggleStyle = () => {
-        if(myStyle.color === "white"){
-        setStyle({
-                backgroundColor:"white",
-                color: "black"
-        });
-        setbtnText("Enable Dark Mode")
-        }
-        else{
-        setStyle({
-            backgroundColor:"black",
-            color: "white"
-        });
-        setbtnText("Enable Light Mode")
-        }
-    }
-
+ 
   return (
     <div>
         <div className="container mt-5 " >
             <div className="row">
-                <div className="col-md-12 fs-5 about-us" style={myStyle}>
+                <div className="col-md-12 fs-5 about-us" >
                     <h1 className='heading'>About Us</h1>
                     <p>Welcome to Text Utility, your go-to platform for text conversion and manipulation. We are passionate about making text-related tasks simple and accessible for everyone.</p>
                     <p>Our mission is to provide a user-friendly tool that allows you to convert text to upper or lower case, listen to the text using speech synthesis, clear the text input, and copy the results effortlessly.</p>
@@ -40,7 +17,7 @@ export default function About() {
 
             <div className="container mt-5 px-3" >
                 <div className="row ">
-                    <div className="col-md-6 fs-5 feature" style={myStyle}>
+                    <div className="col-md-6 fs-5 feature" >
                         <h2 className='heading'>Features</h2>
                         <ul>
                         <li>Text Conversion to Upper Case</li>
@@ -50,16 +27,14 @@ export default function About() {
                         <li>Copy Text Results</li>
                         </ul>
                 </div>
-                <div className="col-md-6 fs-5 contact-us" style={myStyle}>
+
+                <div className="col-md-6 fs-5 contact-us" >
                     <h2 className='heading'>Contact Us</h2>
                     <p>If you have any questions or feedback, feel free to reach out to us:</p>
                     <p>Email: mandlikavi121@gmail.com</p>
                     <p>Phone: +919370286362</p>
                 </div>
             </div>
-         </div>
-         <div className="m-2">
-         <button className="btn btn-outline-primary mx-2" onClick={toggleStyle}>{btnText}</button>
          </div>
     </div>
   )
