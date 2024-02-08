9187971a3f9a5d24c9132309f109708d9eade564
AvishkarMandlik <mandlikavi121@gmail.com>
2024-02-08 13:20:20 +0530

    using setinterval title change feature added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import TextForm from './components/TextForm/TextForm';
 import Alert from './components/Alert/Alert';
+import About from './components/About/About';
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -24,11 +25,25 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = '#042743';
       showAlert("Dark mode has been Enabled", "success");
+      document.title = "TextUtils Darkmode";
+      setInterval(() => {
+        document.title = "TextUtils is Amazing😍😍";
+      }, 1500);
+      setInterval(() => {
+        document.title = "TextUtils📝🖋️";
+      }, 2000);
     }
     else{
       setMode("light");
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been Enabled", "success");
+      document.title = "TextUtils";
+      setInterval(() => {
+        document.title = "TextUtils is Amazing😍😍";
+      }, 1500);
+      setInterval(() => {
+        document.title = "TextUtils📝🖋️";
+      }, 2000);
     }
   }
 
@@ -37,8 +52,8 @@ function App() {
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     <Alert alert={alert}/>
     <div className="container">
-     <TextForm heading="Enter the Text to Analyze Below" mode={mode}/>
-     {/* <About /> */}
+     <TextForm heading="Enter the Text to Analyze Below" mode={mode} showAlert={showAlert}/>
+     <About mode={mode}/>
     </div>
    </>
   );
